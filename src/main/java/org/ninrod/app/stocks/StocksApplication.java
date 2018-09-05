1654b7cac65a10f0e8a241d3f46c017e989bd895
ninrod <filipe.silva@gmail.com>
2018-09-05 09:23:49 -0300

    initial commit

---
+++ src/main/java/org/ninrod/app/stocks/StocksApplication.java
@@ -0,0 +1,11 @@
+package org.ninrod.app.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StocksApplication.class, args);
+	}
+}
