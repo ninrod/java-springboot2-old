1310ba1fd666fa6edb4e2d318b11f9c5c42c3e39
ninrod <filipe.silva@gmail.com>
2018-09-05 09:41:28 -0300

    add working controller test

---
+++ src/test/java/org/ninrod/app/stocks/rest/ControllerTest.java
@@ -0,0 +1,28 @@
+package org.ninrod.app.stocks.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
+public class ControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void test() throws Exception {
+        mvc.perform(get("/greeting").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.content", is("Hello, World!")));
+    }
+}
