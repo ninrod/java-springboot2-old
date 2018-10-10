d1d7f8a247701d1d31d8f8d82236c1b79f161029
ninrod <filipe.silva@gmail.com>
2018-10-10 08:58:56 -0300

    use requestmapping on class

--- src/main/java/org/ninrod/app/stocks/rest/GreetingController.java
+++ src/main/java/org/ninrod/app/stocks/rest/GreetingController.java
@@ -4,11 +4,13 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.ninrod.app.stocks.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("greeting")
 public class GreetingController {
 
     private final AtomicLong counter = new AtomicLong();
@@ -20,7 +22,7 @@ public class GreetingController {
         this.service = service;
     }
 
-    @RequestMapping("/greeting")
+    @GetMapping(produces = "application/json")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(), service.phrase() + name + "!");
     }
